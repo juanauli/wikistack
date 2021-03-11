12de31e917dd9b17484c788020b83fd64b420ec2
Juan Auli <juanseauly@gmail.com>
2021-03-11 15:23:08 -0500

    conected db

--- app.js
+++ app.js
@@ -3,16 +3,13 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const layout = require('./views/layout')
-const { db } = require('./models');
+const { db, Page, User } = require('./models');
 
 db.authenticate()
   .then(() => {
     console.log('connected to the database');
-  })
+  });
 
- const Page = db.define(
-   
- )
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "public"));
 
@@ -20,8 +17,16 @@ app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res, next) => {
   res.send(layout(""));
 });
-const PORT = 3000;
 
-app.listen(PORT, () => {
+const connect = async () => {
+  await db.sync({force: true});
+
+  const PORT = 3000;
+
+  app.listen(PORT, () => {
   console.log(`App listening in port--> http://localhost:${PORT}`)
-})
+});
+};
+
+connect();
+
